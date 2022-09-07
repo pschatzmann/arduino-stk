c7a7fede67bf0f4f69a54cfc29ec89f63562ef31
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-09-07 10:20:09 +0200

    I2S Compile error

--- src/ArdI2SOut.h
+++ src/ArdI2SOut.h
@@ -33,7 +33,7 @@ typedef int (*RtESPAudioCallback)( void *outputBuffer,
     .sample_rate = SAMPLE_RATE, // corrected by info from bluetooth
     .bits_per_sample = (i2s_bits_per_sample_t) 16, 
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = 0, // default interrupt priority
     .dma_buf_count = 8,
     .dma_buf_len = 64,
